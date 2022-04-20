8628ef05bfd3a7c5170764b9bb52e86776941d7e
ayeAlvo <ayealvo@gmail.com>
2022-04-20 00:49:36 -0300

    validaciones para la empresa

--- src/main/java/com/compu/mundo/facturacion/controller/CompanyController.java
+++ src/main/java/com/compu/mundo/facturacion/controller/CompanyController.java
@@ -1,10 +1,15 @@
 package com.compu.mundo.facturacion.controller;
 
 import com.compu.mundo.facturacion.entity.Company;
+import com.compu.mundo.facturacion.exception.CustomException;
 import com.compu.mundo.facturacion.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -24,7 +29,16 @@ public class CompanyController {
     }
 
     @PostMapping("/newCompany")
-    public Company createCompany(@RequestBody Company company){
+    public Company createCompany(@RequestBody @Valid Company company, BindingResult result){
+        if(result.hasErrors()){
+            String message = "";
+            for (ObjectError error : result.getAllErrors()){
+                FieldError fieldError = (FieldError) error;
+                String field = fieldError.getField();
+                message += String.format("%s: %s | ", field, error.getDefaultMessage());
+            }
+            throw new CustomException(message);
+        }
         return companyService.create(company);
     }
 
--- src/main/java/com/compu/mundo/facturacion/entity/Company.java
+++ src/main/java/com/compu/mundo/facturacion/entity/Company.java
@@ -3,8 +3,10 @@ package com.compu.mundo.facturacion.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Data
@@ -19,8 +21,12 @@ public class Company {
     Long id;
 
     @Column(name = "NAME")
+    @NotEmpty(message = "El nombre no puede estar vacio")
+    @Length(min=3,max=150, message = "El nombre debe tener mas de 3 caracteres y menos de 150")
     String name;
 
     @Column(name = "ITEM")
+    @NotEmpty(message = "El rubro no puede estar vacio")
+    @Length(min=3,max=150, message = "El rubro debe tener mas de 3 caracteres y menos de 150")
     String item;
 }
